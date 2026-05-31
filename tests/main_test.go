05d355364b8f7d8434119980b4e86ea2a462ba74
j.alimohammadi <j.alimohammadi@emofid.com>
2026-05-31 18:55:30 +0330

    some messages

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,4 +1,4 @@
-package api_tests
+package tests
 
 import (
 	"log/slog"
@@ -12,16 +12,16 @@ import (
 
 func TestMain(m *testing.M) {
 	logger := slog.Default()
-	
+
 	// Set test database DSN
 	testDBDSN := "postgres://postgres:postgres@localhost/myblog_test?sslmode=disable"
 	os.Setenv("DB_DSN", testDBDSN)
 	os.Setenv("JWT_SECRET", "test-secret-key")
-	
+
 	// Create test database
 	logger.Info("Creating test database...")
 	test_utils.CreateTestDB()
-	
+
 	// Connect to test database
 	logger.Info("Connecting to test database...")
 	db, err := databaseutils.OpenDBConnection(logger, testDBDSN)
@@ -30,7 +30,7 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 	defer db.Close()
-	
+
 	// Run migrations
 	logger.Info("Running migrations on test database...")
 	projectRoot := "/home/javad/Project/go-sample/blog-app/my-blog-app"
@@ -39,15 +39,15 @@ func TestMain(m *testing.M) {
 		logger.Error("Failed to run migrations", "error", err)
 		os.Exit(1)
 	}
-	
+
 	// Setup test database connection for test utilities
 	test_utils.SetupTestDB()
-	
+
 	// Run tests
 	code := m.Run()
-	
+
 	// Cleanup
 	test_utils.TearDownTestDB()
-	
+
 	os.Exit(code)
 }
